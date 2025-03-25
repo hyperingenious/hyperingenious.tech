561e4e7d7ffbc9ed0eef68710b38ce4c2460e995
hyperingenious <skbmasale941@gmail.com>
2025-03-26 00:46:17 +0530

    ADD: bahinchod added

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const quote_wrapper = document.querySelector('.wrapper');
 setInterval(() => {
   quote_wrapper.style.display = 'none'
 }, 2500);
+
+const clickSound = new Audio("bahinchod.mpeg");
+
+document.querySelector('.bahinchod').addEventListener('click', () => clickSound.play())
