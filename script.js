9d7e9496361b50adcd30754b9f6f7802b778c985
hyperingenious <skbmasale941@gmail.com>
2025-03-26 01:46:25 +0530

    ADD: baahinchod rap added

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ setInterval(() => {
   quote_wrapper.style.display = 'none'
 }, 2500);
 
-const clickSound = new Audio("bahinchod.mpeg");
+const clickSound = new Audio("baahinchod.mp3");
 
 document.querySelector('.bahinchod').addEventListener('click', () => clickSound.play())
